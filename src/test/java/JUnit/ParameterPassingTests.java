573b41b99a1cb979c5fab9e0b7aeb3e6668a74f0
Krishna <jagge49@gamail.com>
2024-04-13 17:23:49 +0530

    Passing value form Different Method

--- src/test/java/JUnit/ParameterPassingTests.java
+++ src/test/java/JUnit/ParameterPassingTests.java
@@ -1,9 +1,17 @@
 package JUnit;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ParameterPassingTests {
 
 
@@ -27,7 +35,7 @@ public class ParameterPassingTests {
     @EmptySource//Passing Empty at stating only in String
     // or @NullAndEmptySource
     @ValueSource(strings ={"Hello","World","of","God"}) // This annotation used to tell method which data type, how much value we pass and what value we pass to method
-        // for multiple input we use array {,,,} and specify data type like ints boolean string
+    // 💯 for multiple input we use array {,,,} and specify data type like ints boolean string
     void strValues1(String theParams){
         System.out.println("The args are : "+theParams);
     }
@@ -44,7 +52,7 @@ public class ParameterPassingTests {
         System.out.println("Name = " + param1 + ", Power = " + param2 + ", SuperHero = " + param3);
     }
 
-    //if we wnt to pass comma along with string
+    // 💫 if we want to pass comma along with string
     @ParameterizedTest
     @CsvSource(value={"Steve,'rogre,s'","winter ,"+"'solger,s'"})
     void csv_with_comma(String params1,String params2){
@@ -78,6 +86,43 @@ public class ParameterPassingTests {
                 ", qty = " + qty + ", uom = " + uom + ", provider = " + provider);
     }
 
-    // in csv if we want empty string enter "" if we want null leave it blank
+    // 💥 in csv if we want empty string enter "" if we want null leave it blank
+
+    @ParameterizedTest
+    @MethodSource(value="sourceStringAsStream") // pass which ever method we want in below method
+    void methodSource_String(String vegetable){
+        System.out.println("vegetable = " + vegetable);
+    }
+    //🐞 if they are in different class we need to make it as Static 🐞
+    // @MethodSource(value = "junit5tests.ParamProvider#sourceStream_StringDouble")
+    // package.method we need to pass
+    List<String> sourceString(){
+        //data processing
+        return Arrays.asList("tomato","carrot","cabbage");
+    }
+
+    //we can also pass it as Stream or List od array
+    Stream<String> sourceStringAsStream(){
+        //data processing
+        return Stream.of("Meat","Milk","egg");
+    }
+
+    //method pass both int and string at same method
+
+    @ParameterizedTest
+    @MethodSource(value="sourceStream_StringDouble") // pass which ever method we want in below method
+    void methodSource_StringIntInt(String vegetable,int qty,int price){
+        System.out.println("vegetable = " + vegetable + ", qty = " + qty + ", price = " + price);
+    }
+    List<Arguments> sourceList_StringIntInt(){
+        return Arrays.asList(arguments("carrot",2,10), arguments("beetroot",4,18), arguments("potato",9,90));
+    }
+
+    Stream<Arguments> sourceStream_StringDouble() {
+        //processing
+        return Stream.of(arguments("meat", 2, 0),
+                arguments("egg", 4, 5), arguments(
+                        "milk", 7, 8));
+    }
 
 }
